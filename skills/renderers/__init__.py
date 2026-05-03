9abf71f0437b01c8362099fdb7bc979e97fe23c9
trevormentis-spec <trevor.mentis@gmail.com>
2026-05-02 20:44:36 -0700

    add intel visual renderer package

---
+++ skills/renderers/__init__.py
@@ -0,0 +1,5 @@
+"""Deterministic renderers for OpenClaw magazine/intelligence visuals."""
+
+from .intel_svg_renderer import IntelSVGRenderer, VisualQualityError
+
+__all__ = ["IntelSVGRenderer", "VisualQualityError"]
