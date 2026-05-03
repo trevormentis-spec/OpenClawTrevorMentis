c861b5df3942b6a007f805fd2be4c7a6f7727b0c
trevormentis-spec <trevor.mentis@gmail.com>
2026-05-02 22:06:48 -0700

    add visual production package

---
+++ skills/visual_production/__init__.py
@@ -0,0 +1,10 @@
+from .pipeline import MagazineVisualProductionSkill
+from .schemas import SourceContent, ProducedVisual, VisualProductionPlan, VisualBrief
+
+__all__ = [
+    "MagazineVisualProductionSkill",
+    "SourceContent",
+    "ProducedVisual",
+    "VisualProductionPlan",
+    "VisualBrief",
+]
